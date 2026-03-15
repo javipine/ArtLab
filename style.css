fed3cf8c0f888c6f41f2e3ee745e39cce756a229
javipine <javierenriquepinedagonzalez5@gmail.com>
2026-03-14 22:10:05 -0400

    Add styles for canvas and print overlay

--- style.css
+++ style.css
@@ -1,1 +1,29 @@
+.canvas-prenda {
+  position: relative;
+  width: 400px;
+  height: 500px;
+  border: 2px dashed #ccc;
+  margin: 0 auto;
+  overflow: hidden;
+}
 
+.base-item {
+  width: 100%;
+  z-index: 1;
+}
+
+.print-overlay {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 150px; /* Tamaño del área de impresión */
+  height: 150px;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+}
